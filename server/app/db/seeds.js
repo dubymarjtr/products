6c7524b1bb42063b6ccc29c3e3d84cd1a4f79a4f
Dubymar Tollinchi <dubymarjtr@gmail.com>
2021-10-20 15:16:54 -0500

    Seed the database

--- server/app/db/seeds.js
+++ server/app/db/seeds.js
@@ -1,3 +1,11 @@
-import products from "./data.js";
+import once from "./conns/once.js";
+import productsData from "./data.js";
 
-console.log(products);
+once
+  .connect()
+  .then((connection) =>
+    connection.db("products").collection("products").insertMany(productsData)
+  )
+  .then(() => {
+    once.close();
+  });
