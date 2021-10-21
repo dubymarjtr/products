bda37797c14e6e94f28d0fe0fb3538c3189b9237
Dubymar Tollinchi <dubymarjtr@gmail.com>
2021-10-20 19:48:56 -0500

    feat: :sparkles: create "Hello from API" router for testing

--- server/app/router.js
+++ server/app/router.js
@@ -1,7 +1,22 @@
 import Router from "express";
+import config from "./config.js";
+import client from "./db/conns/client.js";
+
+const {
+  db: { name, collectionName: collection },
+} = config;
 
 const router = new Router();
 
-// TODO: Add routes here (maybe 🤔 start with a GET test route)
+// localhost:3000/api
+router.get("/", (_, res) => {
+  res.send("Hello from API router");
+});
+
+// localhost:3000/products
+router.get("/products", async (_, res) => {
+  const products = await client.db(name).collection(collection).find({});
+  res.json(products);
+});
 
 export default router;
